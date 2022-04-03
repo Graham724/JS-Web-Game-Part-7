9b981fe336f8d3013aa93866c0f4e00c49149816
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-04-03 11:18:53 -0500

    Second commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-// npc.walkEast(2000)
 async function moveNPC(){
     await npc.walkNorth(1400)
     await npc.walkEast(1200)
